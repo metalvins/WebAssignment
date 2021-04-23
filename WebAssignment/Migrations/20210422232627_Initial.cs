f32ca207857eff188eba50b03c9fae90c3e7291c
Vinesh Thakur <vineshthakur@gmail.com>
2021-04-24 05:07:00 +0530

    * 20210422232627_Initial.cs: Missed adding folder to git
    * DataContextModelSnapshot.cs:

---
+++ WebAssignment/Migrations/20210422232627_Initial.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAssignment.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    AuthorID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fname = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Lname = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Bday = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    Website = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: true),
+                    Phone = table.Column<string>(type: "TEXT", maxLength: 20, nullable: true),
+                    Country = table.Column<string>(type: "TEXT", maxLength: 70, nullable: true),
+                    Province = table.Column<string>(type: "TEXT", maxLength: 70, nullable: true),
+                    Zip = table.Column<string>(type: "TEXT", maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.AuthorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    BlogID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true),
+                    Content = table.Column<string>(type: "TEXT", maxLength: 5000, nullable: true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    AuthorID = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.BlogID);
+                    table.ForeignKey(
+                        name: "FK_Blogs_Authors_AuthorID",
+                        column: x => x.AuthorID,
+                        principalTable: "Authors",
+                        principalColumn: "AuthorID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_AuthorID",
+                table: "Blogs",
+                column: "AuthorID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
