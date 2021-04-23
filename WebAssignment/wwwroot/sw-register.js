c0ba4375bdce85d25f365720caee7e11d02ca8ad
Vinesh Thakur <vineshthakur@gmail.com>
2021-04-23 07:24:31 +0530

    * sw.js:
    * sw-register.js: Add cache and service worker

--- WebAssignment/wwwroot/sw-register.js
+++ WebAssignment/wwwroot/sw-register.js
@@ -1,4 +1,19 @@
-﻿if ('serviceWorker' in navigator) {
+﻿// This is the "Offline page" service worker
+
+// Add this below content to your HTML page inside a <script type="module"></script> tag, or add the js file to your page at the very top to register service worker
+// If you get an error about not being able to import, double check that you have type="module" on your <script /> tag
+
+/*
+ This code uses the pwa-update web component https://github.com/pwa-builder/pwa-update to register your service worker,
+ tell the user when there is an update available and let the user know when your PWA is ready to use offline.
+*/
+
+import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+
+const el = document.createElement('pwa-update');
+document.body.appendChild(el);
+
+if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
         this.navigator.serviceWorker.register('/sw.js').then(function (registration) {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
--- WebAssignment/wwwroot/sw.js
+++ WebAssignment/wwwroot/sw.js
@@ -1,3 +1,88 @@
-﻿self.addEventListener('install', (event) => {
-    console.log('Service Worker installing...');
+﻿// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
 });
+
+self.addEventListener('install', async (event) => {
+    event.waitUntil(
+        caches.open(CACHE)
+            .then((cache) => cache.add(offlineFallbackPage))
+    );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+    workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith((async () => {
+            try {
+                const preloadResp = await event.preloadResponse;
+
+                if (preloadResp) {
+                    return preloadResp;
+                }
+
+                const networkResp = await fetch(event.request);
+                return networkResp;
+            } catch (error) {
+
+                const cache = await caches.open(CACHE);
+                const cachedResp = await cache.match(offlineFallbackPage);
+                return cachedResp;
+            }
+        })());
+    }
+});
+
+
+const cache_name = 'my-cache';
+let urlsToCache = [
+    '/css/site.css',
+    '/js/site.js',
+    '/WebAssignment/Pages/Index.cshtml'
+];
+
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing...');
+
+    event.waitUntil(
+        caches.open(cache_name)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('Service Worker installing...');
+
+    event.respondsWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (response) {
+                    return response;
+                }
+                const netResponse = fetch(event.request);
+                caches.open(cache_name).then((cache) => {
+                    cache.add(event.request);
+                });
+                return netResponse;
+            }
+            )
+    );
+});
+
+ 
